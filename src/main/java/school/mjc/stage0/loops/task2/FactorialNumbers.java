e774e68a246b9ea253144b1056a424771ee29b62
Sunkar <sunkar2710@gmail.com>
2023-04-22 14:24:22 +0600

    feat: implement stage0-module4-loops-task2

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int counter = 0;
+        while (counter <= printToInclusive) {
+            int fac = 1, i = 1;
+            while (i <= counter) {
+                fac *= i++;
+            }
+            System.out.println(fac);
+            counter++;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 2;
+        while (counter <= printToInclusive) {
+            if (counter > 1) {
+                boolean isPrime = true;
+                int i = 2;
+                while (i <= Math.sqrt(counter)) {
+                    if (counter % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (isPrime) System.out.println(counter);
+            }
+            counter++;
+        }
     }
 }
